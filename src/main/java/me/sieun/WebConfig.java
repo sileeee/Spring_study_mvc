2ef7ca3c09d426ab4b44f795324245942a41b32e
sileeee <tldms201@naver.com>
2021-06-05 19:10:37 +0900

    Link Spring MVC

--- src/main/java/me/sieun/WebConfig.java
+++ src/main/java/me/sieun/WebConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(excludeFilters = @ComponentScan.Filter(HelloController.class))
-public class AppConfig {
+@ComponentScan(useDefaultFilters = false, includeFilters = @ComponentScan.Filter(HelloController.class))
+public class WebConfig {
 
 }
